69b159d527f99c42e152f8b09e48dd4d4fc05ed1
Vladislav <haskicbr@gmail.com>
2023-07-15 20:02:14 +0300

    add vehicle

---
+++ Source/TPS/Classes/Pawns/WheelPawn.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "WheelPawn.generated.h"
+
+UCLASS(Blueprintable)
+class TPS_API AWheelPawn : public AWheeledVehiclePawn
+{
+  GENERATED_BODY()
+};
